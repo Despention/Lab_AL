aa0295e865d1358030194b45caf627fa450316a6
Despention <115477924+Despention@users.noreply.github.com>
2023-10-05 07:21:49 -0400

    Update lab2_3.cpp

--- lab2_3.cpp
+++ lab2_3.cpp
@@ -11,7 +11,10 @@ struct Node {
 };
 
 struct LinkedList {
+private:
     Node* head;
+
+public:
     LinkedList() : head(nullptr) {}
 
     void addAtHead(int val) {
@@ -33,35 +36,54 @@ struct LinkedList {
         Node* prev = nullptr;
         Node* current = head;
         Node* next = nullptr;
-        
+
         while (current != nullptr) {
-            next = (*current).next; 
-            (*current).next = prev; 
-            prev = current; 
-            current = next; 
+            next = (*current).next;
+            (*current).next = prev;
+            prev = current;
+            current = next;
         }
-        
-        head = prev; 
+
+        head = prev;
+    }
+
+private:
+    void xReverse(Node* current) {
+        if (current == nullptr) {
+            return;
+        }
+        xReverse((*current).next);
+        cout << (*current).val << " ";
+    }
+
+public:
+    void printReverse() {
+        cout << "Обратный лист:\n";
+        xReverse(head);
+        cout << endl;
     }
 };
 
 int main() {
     setlocale(LC_ALL,"Rus");
 
-    srand(time(0)); 
-    
-    LinkedList list; 
-    
-    for(int i = 0; i < 10; i++) {
-        int randomValue = rand() % 100; 
+    srand(time(0));
+
+    LinkedList list;
+
+    for (int i = 0; i < 10; i++) {
+        int randomValue = rand() % 100;
         list.addAtHead(randomValue);
     }
-    
-    cout << "Изначальный лист\n ";
-    list.printList(); 
-    
-    list.reverse(); 
-    
-    cout << "Перевернутый лист:\n ";
-    list.printList(); 
+
+    cout << "Изначальный лист:\n";
+    list.printList();
+
+    list.reverse();
+
+    cout << "Перевернутый лист:\n";
+    list.printList();
+
+    cout << "2x Перевернутый лист:\n";
+    list.printReverse();
 }
