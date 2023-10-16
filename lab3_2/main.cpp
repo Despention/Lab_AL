2b528cc545362f3f01d87197b75d767e8e89f158
Despention <115477924+Despention@users.noreply.github.com>
2023-10-16 03:14:26 -0400

    Update main.cpp

--- lab3_2/main.cpp
+++ lab3_2/main.cpp
@@ -3,13 +3,20 @@
 using namespace std;
 
 int main() {
-    Stack<int> stack;
+    Stack stack;  
 
     stack.push(1);
     stack.push(2);
     stack.push(3);
 
     cout << "Top of the stack: " << stack.top() << endl;
+    cout << "Stack contents: ";
+    Stack tempStack = stack;  
+    while (!tempStack.isEmpty()) {
+        cout << tempStack.top() << " ";
+        tempStack.pop();
+    }
+    cout << endl;
 
     stack.pop();
     cout << "Popped the top element." << endl;
